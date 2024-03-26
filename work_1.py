b49176ea505cab4b931df10829e10fb75c561132
Serhii Chervonospako <novikatmospro@gmail.com>
2024-03-26 11:48:22 +0100

    Перездаю 1,2,4 без змін

--- work_1.py
+++ work_1.py
@@ -1,22 +1,21 @@
-def caching_fibonacci(n):
-    cache ={}
-    Debug_Test = False
+def caching_fibonacci():
+    
+    cache = {}
     def fibonacci(n):
         if n <= 0:
             return 0
+        
         if n == 1:
             return 1
-        if n in cache:
-            if Debug_Test == True:
-                print(f'Easy work: {n}')
-            return cache[n]
 
+        if n in cache:
+            return cache[n]
+        
         cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
-        if Debug_Test == True:
-            print(f'Hard work: {n}')
         return cache[n]
         
+    return fibonacci
 
-    return fibonacci(n)
-fibo = caching_fibonacci(n=9)
-print(fibo)
+fibo = caching_fibonacci()
+print(fibo(10))
+print(fibo(15))
--- work_2.py
+++ work_2.py
@@ -8,11 +8,11 @@ def generator_numbers(text: str):
 
 def sum_profit(text: str, func:Callable):
     total_sum = 0
-    for el in func:
+    for el in func(text):
         total_sum += el
     return total_sum
 
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
-total_income = sum_profit(text, func=generator_numbers(text))
+total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
